3a35810d9ba325779cfb19689f6f751928d80e4a
Alexander Lloyd <axl639@student.bham.ac.uk>
2018-12-11 18:08:34 +0000

    Add Unit tests for ConfigController

--- config-service/src/test/java/com/alexlloyd/configservice/controller/ConfigControllerTest.java
+++ config-service/src/test/java/com/alexlloyd/configservice/controller/ConfigControllerTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
@@ -39,6 +41,39 @@ class ConfigControllerTest {
     private ConfigService configService;
 
     @Test
+    @DisplayName("should return a list of all the configs")
+    void testGetConfigList() throws Exception {
+        when(configService.getConfigNames()).thenReturn(Collections.singleton(CONFIG_NAME));
+
+        mockMvc.perform(get("/config"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.response", is("SUCCESS")))
+                .andExpect(jsonPath("$.errors").doesNotExist())
+                .andExpect(jsonPath("$.data").isMap())
+                .andExpect(jsonPath("$.data.configs").isArray())
+                .andExpect(jsonPath("$.data.configs", hasSize(1)))
+                .andExpect(jsonPath("$.data.configs[0]", is(CONFIG_NAME)));
+    }
+
+    @Test
+    @DisplayName("should return an empty list of configs when there are not any")
+    void testGetConfigListEmptyList() throws Exception {
+        when(configService.getConfigNames()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/config"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.response", is("SUCCESS")))
+                .andExpect(jsonPath("$.errors").doesNotExist())
+                .andExpect(jsonPath("$.data").isMap())
+                .andExpect(jsonPath("$.data.configs").isArray())
+                .andExpect(jsonPath("$.data.configs", hasSize(0)));
+    }
+
+    @Test
     @DisplayName("should get a config")
     void testGetConfig() throws Exception {
         when(configService.getConfig(eq(CONFIG_NAME))).thenReturn(CONFIG);
@@ -51,14 +86,21 @@ class ConfigControllerTest {
                 .andExpect(jsonPath("$.errors").doesNotExist())
                 .andExpect(jsonPath("$.data").isMap())
                 .andExpect(jsonPath("$.data.config").isMap())
-                .andExpect(jsonPath("$.data.config."+KEY).value(is(VALUE)));
+                .andExpect(jsonPath("$.data.config." + KEY).value(is(VALUE)));
     }
 
     @Test
     @DisplayName("should create a config")
     void testCreateConfig() throws Exception {
         mockMvc.perform(post("/config/" + CONFIG_NAME))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.response", is("SUCCESS")))
+                .andExpect(jsonPath("$.errors").doesNotExist())
+                .andExpect(jsonPath("$.data").isString())
+                .andExpect(jsonPath("$.data", containsString(CONFIG_NAME)));
+
 
         verify(configService).createConfig(CONFIG_NAME);
     }
@@ -66,9 +108,16 @@ class ConfigControllerTest {
     @Test
     @DisplayName("should thrown an Exception if Config already exists")
     void testCreateConfigError() throws Exception {
-        doThrow(ConfigAlreadyExistsException.class).when(configService).createConfig(eq(CONFIG_NAME));
+        doThrow(new ConfigAlreadyExistsException(CONFIG_NAME)).when(configService).createConfig(eq(CONFIG_NAME));
 
         mockMvc.perform(post("/config/" + CONFIG_NAME))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.response", is("FAILURE")))
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0].reason", containsString("exists")))
+                .andExpect(jsonPath("$.data").doesNotExist());
     }
 }
