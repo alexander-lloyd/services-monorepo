19dbf845a241272154e983ac8be57d7f5b1e1029
Alexander Lloyd <axl639@student.bham.ac.uk>
2018-12-23 18:48:53 +0000

    Address Static Analysis comments

--- config-service/src/test/java/com/alexlloyd/configservice/controller/FaviconTest.java
+++ config-service/src/test/java/com/alexlloyd/configservice/controller/FaviconTest.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.isEmptyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,10 +28,13 @@ class FaviconTest {
 
     @Test
     @DisplayName("should not have a favicon")
-    void testNoFavicon() throws Exception {
-
-        mockMvc.perform(get(FAVICON_URL))
+    public void testNoFavicon() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get(FAVICON_URL))
                 .andDo(print())
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getContentAsString(), isEmptyString());
     }
 }
