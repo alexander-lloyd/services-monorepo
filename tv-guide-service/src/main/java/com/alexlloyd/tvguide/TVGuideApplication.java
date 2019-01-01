0a4eb9e039b6d0e83b9084900d9634bccebb68d4
Alexander Lloyd <axl639@student.bham.ac.uk>
2019-01-01 17:50:39 +0000

    Initial commit of TV Guide Service

---
+++ tv-guide-service/src/main/java/com/alexlloyd/tvguide/TVGuideApplication.java
@@ -0,0 +1,12 @@
+package com.alexlloyd.tvguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TVGuideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TVGuideApplication.class, args);
+    }
+
+}
