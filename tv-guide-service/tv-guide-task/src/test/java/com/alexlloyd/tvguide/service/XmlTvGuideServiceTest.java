90d1179a71217705c5957addcd27451bbda8c4ab
Alexander Lloyd <axl639@student.bham.ac.uk>
2019-02-03 16:51:14 +0000

    Add ChannelIconMapperTest

--- tv-guide-service/tv-guide-task/src/test/java/com/alexlloyd/tvguide/service/XmlTvGuideServiceTest.java
+++ tv-guide-service/tv-guide-task/src/test/java/com/alexlloyd/tvguide/service/XmlTvGuideServiceTest.java
@@ -5,8 +5,9 @@ import java.util.Set;
 import com.alexlloyd.tvguide.api.TvGuideService;
 import com.alexlloyd.tvguide.mapper.ChannelIconMapper;
 import com.alexlloyd.tvguide.mapper.ChannelMapper;
+import com.alexlloyd.tvguide.mapper.ProgrammeMapperTest;
+import com.alexlloyd.tvguide.model.Channel;
 import com.alexlloyd.tvguide.model.GuideWrapper;
-
 import com.alexlloyd.tvguide.model.XmlTvChannel;
 import com.alexlloyd.tvguide.model.XmlTvProgramme;
 import com.alexlloyd.tvguide.task.TvGuideTask;
@@ -25,6 +26,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class)
 @ExtendWith(SpringExtension.class)
@@ -39,28 +41,34 @@ public class XmlTvGuideServiceTest {
     @Autowired
     private TvGuideTask tvGuideTask;
 
+    @Autowired
+    private ChannelMapper channelMapper;
+
+    @Autowired
+    private ProgrammeMapperTest programmeMapper;
+
     @Test
     public void testGetTvGuide() {
-        XmlTvProgramme programme = new XmlTvProgramme.Builder()
+        XmlTvProgramme xmlProgramme = new XmlTvProgramme.Builder()
                 .setChannelId("def")
                 .setTitle("Programme Name")
                 .setDescription("Programme Description")
                 .build();
-        XmlTvChannel channel = new XmlTvChannel.Builder()
+        XmlTvChannel xmlChannel = new XmlTvChannel.Builder()
                 .setId("abc")
                 .setName("Channel Name")
                 .build();
 
         GuideWrapper wrapper = new GuideWrapper();
-        wrapper.setProgrammes(Set.of(programme));
-        wrapper.setChannels(Set.of(channel));
+        wrapper.setProgrammes(Set.of(xmlProgramme));
+        wrapper.setChannels(Set.of(xmlChannel));
 
         doReturn(ResponseEntity.status(200).body(wrapper)).when(restTemplate).getForEntity(anyString(), any(), anyString());
 
         // Method call
         this.tvGuideTask.getAll();
+        verify(tvGuideService).saveChannel(any(Channel.class));
 
-//        verify(tvGuideService).saveTvGuide(eq(wrapper));
     }
 
     @Configuration
@@ -76,20 +84,24 @@ public class XmlTvGuideServiceTest {
         }
 
         @Bean
-        public ChannelMapper channelMapper() {
-            return mock(ChannelMapper.class);
+        public ProgrammeMapperTest programmeMapper() {
+            return new ProgrammeMapperTest();
+        }
+
+        @Autowired
+        @Bean
+        public ChannelMapper channelMapper(ChannelIconMapper channelIconMapper) {
+            return new ChannelMapper(channelIconMapper);
         }
 
         @Bean
         public ChannelIconMapper channelIconMapper() {
-            return mock(ChannelIconMapper.class);
+            return new ChannelIconMapper();
         }
 
         @Autowired
         @Bean
         public TvGuideTask tvGuideTask(RestTemplate restTemplate, TvGuideService tvGuideService, ChannelMapper channelMapper) {
-            System.out.print("ASD ");
-            System.out.println(restTemplate);
             return new TvGuideTask(restTemplate, tvGuideService, channelMapper);
         }
     }
